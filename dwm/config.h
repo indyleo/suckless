72d2f95fb60a8b1598737e4c8e1aba58eec8c42a
Indy <Indy@debian.org>
2025-04-19 12:03:17 -0400

    wallpaper key

--- dwm/config.h
+++ dwm/config.h
@@ -145,7 +145,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("dmenu_flatpak") },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|SHIFTKEY,              XK_w,      spawn,          SHCMD("xwalr $HOME/Pictures/Wallpapers") },
+	{ MODKEY|SHIFTKEY,              XK_w,      spawn,          SHCMD("xwalr ~/Pictures/Wallpapers") },
 
   /* Alt Tab */
   { ALTKEY,                       XK_Tab,    spawn,          SHCMD("dmenu_alttab") },
