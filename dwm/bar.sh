f2391e1c0e7e4fae83bb646394d13faec5da816f
Indy <Indy@debian.org>
2025-10-14 19:53:01 -0400

    update

--- dwm/bar.sh
+++ dwm/bar.sh
@@ -1,6 +1,7 @@
 #!/bin/env dash
 
 while true; do
+    isstream=$(sysstats isstream)
     isrec=$(sysstats isrec)
     # browsers=$(browserctrl -bst 25)   # disabled in your config
     song=$(songctrl -sst SubsonicTUI 20)
@@ -9,8 +10,8 @@ while true; do
     bat=$(sysstats battery)
     datetime=$(sysstats date_time)
 
-    # xsetroot -name " $isrec || $browsers ||  $song || $bright || $vol || $bat || $datetime "
-    xsetroot -name " $isrec || $song || $bright || $vol || $bat || $datetime "
+    # xsetroot -name " $isstream || $isrec || $browsers ||  $song || $bright || $vol || $bat || $datetime "
+    xsetroot -name " $isstream || $isrec || $song || $bright || $vol || $bat || $datetime "
 
     sleep 0.2
 done
