4c961046ff196c44b3bfd1286c03e07c0ea6bc2c
Indy <Indy@debian.org>
2025-11-16 12:45:39 -0500

    fix

--- dwmblocks-async/config.def.h
+++ dwmblocks-async/config.def.h
@@ -17,12 +17,11 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)                                \
-    X("", "sysstats ismedia", 0, 20)             \
-    X("", "browserctrl -bst 20", 0, 21)          \
-    X("", "songctrl -sst SubsonicTUI 25", 0, 22) \
-    X("", "sysstats volume", 0, 23)              \
-    X("", "sysstats brightness", 0, 24)          \
-    X("", "sysstats battery", 15, 0)             \
+#define BLOCKS(X)                           \
+    X("", "sysstats ismedia", 0, 20)        \
+    X("", "mediactl state-title 35", 0, 22) \
+    X("", "sysstats volume", 0, 23)         \
+    X("", "sysstats brightness", 0, 24)     \
+    X("", "sysstats battery", 15, 0)        \
     X("", "sysstats date_time", 30, 0)
 #endif  // CONFIG_H
