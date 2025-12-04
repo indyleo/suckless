45afb64e5865f648ffd5554516752c16a2bffafb
Indy <Indy@debian.org>
2025-12-04 13:01:19 -0500

    added wezterm

--- dwm/config_nord.def.h
+++ dwm/config_nord.def.h
@@ -78,6 +78,7 @@ static const Rule rules[] = {
     {"Firefox", NULL, NULL, 1 << 8, 0, 0, -1, -1},
     {"St", NULL, NULL, 0, 0, 1, 0, -1},
     {"Alacritty", NULL, NULL, 0, 0, 1, 0, -1},
+    {"org.wezfurlong.wezterm", NULL, NULL, 0, 0, 1, 0, -1},
     {NULL, NULL, "Event Tester", 0, 0, 0, 1, -1}, /* xev */
     {"termsc", NULL, NULL, SPTAG(0), 1, -1},
     {"lfsc", NULL, NULL, SPTAG(1), 1, -1},
